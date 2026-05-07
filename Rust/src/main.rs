5afbd2b88933b7ad719d4d4fc1c3f746ea6a7864
FFCrazy <13453488571@163.com>
2026-05-07 19:01:18 +0800

    feat: add rustfmt

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -3,15 +3,5 @@ use std::{collections::HashMap, i32};
 pub struct Solution;
 const MOD: i32 = 109 + 7;
 
-impl Solution {
-    pub fn shift_distance(
-        s: String,
-        t: String,
-        next_cost: Vec<i32>,
-        previous_cost: Vec<i32>,
-    ) -> i64 {
-      
-    }
-}
 
 fn main() {}
